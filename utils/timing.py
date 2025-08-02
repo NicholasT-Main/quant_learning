80dbfbc8df9d8560cc4e05895f9088ed108751c9
Nick T <nick.o.t@icloud.com>
2025-08-02 13:49:45 +0100

    added unpacking option to timer function

--- utils/timing.py
+++ utils/timing.py
@@ -1,7 +1,22 @@
 import time
 
-def timer_function(function):
+
+def timer_function(function, unpack=False):
+    """
+    Times the execution of a function and returns the elapsed time with results.
+
+    Args:
+        function: A callable function with no arguments to be timed
+        unpack: If True, unpacks function results; if False, returns as single value
+
+    Returns:
+        tuple: (time_taken, *results) if unpack=True, (time_taken, results) if unpack=False
+    """
     start_time = time.time()
-    results=function()
-    time_taken=time.time()-start_time
-    return time_taken,results
+    results = function()
+    time_taken = time.time() - start_time
+
+    if unpack:
+        return time_taken, *results
+    else:
+        return time_taken, results
